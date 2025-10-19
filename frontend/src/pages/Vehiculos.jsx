ba20f0b11574ef1bfb74e5f22ebe4ae6962c17eb
valemr05 <valeria.munoz3@utp.edu.co>
2025-10-18 21:52:07 -0500

    frase

--- frontend/src/pages/Vehiculos.jsx
+++ frontend/src/pages/Vehiculos.jsx
@@ -2,7 +2,7 @@ function Vehiculos() {
   return (
     <div className="bg-white rounded-xl shadow-md p-6">
       <h1 className="text-2xl font-bold">Vehículos</h1>
-      <p className="text-gray-600 mt-2">Página en construcción...</p>
+      <p className="text-gray-600 mt-2">Página en construcción jeje...</p>
     </div>
   );
 }
